1a929df13cd3b3357db5e9b8b563539d487878d9
pawaox4th <pawaox4th@gmail.com>
2021-09-09 23:41:27 +0700

    feat: 🎸 Addc condition show concern

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <ModalConCern setIsSelect={setIsSelect} />
+      {!isSelect && <ModalConCern setIsSelect={setIsSelect} />}
     </div>
   )
 }
