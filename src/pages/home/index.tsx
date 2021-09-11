c4a73cb5d1d6abff6d416010c12641b45a340f4a
PawaOx4th <pawaox4th@gmail.com>
2021-09-11 19:03:23 +0700

    feat: 🎸 Add animation turn circle image

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,40 @@
 import React from "react"
+import styled, { keyframes } from "styled-components"
+import ImgCicle from "../../assets/img/cicle.svg"
 
 const Home = () => {
   return (
-    <div className={`bg-black w-full min-h-screen text-white flex justify-end`}>
+    <div
+      className={`bg-black w-full min-h-screen text-white flex justify-end `}>
       {/* <div>1</div> */}
-      <div className={` w-full lg:w-11/12 border border-yellow-300`}>2</div>
+      <div className={` w-full lg:w-11/12 border border-yellow-300 relative`}>
+        <ImageCircle src={ImgCicle} alt="" />
+      </div>
     </div>
   )
 }
+
+const CircleImg = keyframes`
+0% {
+    /* tr */
+    transform: rotate(0deg);
+}
+
+
+100% {
+    transform: rotate(360deg);
+}
+
+`
+
+const ImageCircle = styled.img`
+  animation: ${CircleImg} 10s linear infinite;
+  overflow: hidden;
+  display: block;
+  position: absolute;
+  top: 28px;
+  right: 64px;
+  z-index: 5;
+`
 
 export default Home
