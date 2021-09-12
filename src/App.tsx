c67d64604bc3bd122903ce54a53bf6a4880385bb
PawaOx4th <pawaox4th@gmail.com>
2021-09-12 13:49:40 +0700

    feat: 🎸 Add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import ReactTypingEffect from "react-typing-effect"
 import ModalConCern from "./components/ModalConcern"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import Gift from "./assets/img/gif-load.webp"
 import "./App.css"
 import { Container } from "./components/common"
@@ -27,10 +27,10 @@ function App() {
   useEffect(() => {
     if (isSelect === false) {
       location.href = "https://www.youtube.com/watch?v=nxwY8JfpjKo"
-    } else {
+    } else if (isSelect === true) {
       setTimeout(() => {
         setIsShowMainPage(true)
-      }, 5000)
+      }, 3000)
     }
   }, [isSelect])
 
@@ -40,15 +40,41 @@ function App() {
         <ModalConCern setIsSelect={setIsSelect} />
       ) : (
         <Container
-          className={` bg-black text-white min-h-screen flex justify-center items-center ${
+          className={` bg-black text-white min-h-screen flex flex-col justify-center items-center ${
             isShowMainPage && "hidden"
           }`}>
           <img src={Gift} alt="" width="300px" height="300px" />
+          <Loadding className="cut-1 mt-5"></Loadding>
         </Container>
       )}
       {isShowMainPage && <Home />}
     </div>
   )
 }
+
+const LoaddingAnimation = keyframes`
+ 0%    {-webkit-mask-size:0    0  ,0    0  ,0    0  ,auto}
+  16.67%{-webkit-mask-size:var(--s),0    0  ,0    0  ,auto}
+  33.33%{-webkit-mask-size:var(--s),var(--s),0    0  ,auto}
+  50%   {-webkit-mask-size:var(--s),var(--s),var(--s),auto}
+  66.67%{-webkit-mask-size:0    0  ,var(--s),var(--s),auto}
+  83.33%{-webkit-mask-size:0    0  ,0    0  ,var(--s),auto}
+  100%  {-webkit-mask-size:0    0  ,0    0  ,0    0  ,auto}
+`
+
+const Loadding = styled.div`
+  width: 70px;
+  height: 26px;
+  background: #ffffff;
+  border-radius: 50px;
+  --c: radial-gradient(farthest-side, #000 92%, #0000);
+  --s: 18px 18px;
+  -webkit-mask: var(--c) left 4px top 50%, var(--c) center,
+    var(--c) right 4px top 50%, linear-gradient(#000 0 0);
+  -webkit-mask-composite: xor;
+  mask-composite: exclude;
+  -webkit-mask-repeat: no-repeat;
+  animation: ${LoaddingAnimation} 1.5s infinite;
+`
 
 export default App
