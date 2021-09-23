dbad5b1f12decfa67897c02d57fc19b5eea13d95
Pawaox4th <pawaox4th@gmail.com>
2021-09-23 20:35:08 +0700

    feat: 🎸 Update section content

--- src/pages/home/components/sectionShowWork.tsx
+++ src/pages/home/components/sectionShowWork.tsx
@@ -5,7 +5,8 @@ import SampleImageContent from "../../../../public/Images/sample-bg-content.png"
 function SectionShowWork() {
   return (
     <Container>
-      <section className={`flex flex-col lg:flex-row lg:gap-9 items-start`}>
+      <section
+        className={`flex flex-col lg:flex-row lg:gap-9 items-start sticky w-full h-auto`}>
         <div className={`flex flex-col items-start lg:w-4/12 `}>
           <h4 className={`text-sm lg:text-base font-normal`}>
             Website Design Concept
@@ -18,6 +19,7 @@ function SectionShowWork() {
         <div className={`mt-8 w-full h-72 lg:w-8/12 lg:h-469`}>
           <img
             src={SampleImageContent}
+            loading="lazy"
             alt=""
             className={` w-full h-full object-cover`}
           />
